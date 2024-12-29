593e9b0f987db5b3f04ebe79959e3d752b09c23e
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2024-12-29 21:01:35 +0500

    Fix type

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 
-export function generateTokens({ userId } : { userId: string }, jwtSecret: string, expiresIn: string | number) {
+export function generateTokens({ userId } : { userId: string }, jwtSecret: string, expiresIn: string) {
   return {
     accessToken: jwt.sign({ userId }, jwtSecret, {
       expiresIn
